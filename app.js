f120b5092f5dbe5e95a0047a0098911f7e987a11
NicoSalva30 <nicolas_salva1@hotmail.com>
2023-10-23 13:21:03 -0300

    Agregue las carpetas public, views y cree el servidor con el puerto 3000

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const express = require("express");
+const app= express();
 const PORT= 3000;
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.listen(3000, ()=>{
+    console.log("Conectado al puerto " + PORT)
+});
+
+app.get("/", (req,res)=> {
+    res.sendFile(__dirname + "/views/index.html")
+});
